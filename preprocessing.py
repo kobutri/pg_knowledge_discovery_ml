b9f44338c8c6923022402fe4dbb3c57d11a172d2
Tristan Kobusch <tristan.kobusch@gmail.com>
2023-03-01 17:05:22 +0100

    preprocessing example

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from nltk.tokenize import NLTKWordTokenizer
+import json
 
 def preprocess(path):
     """Return preprocessed dataframe
@@ -15,8 +17,20 @@ def preprocess(path):
 
         ======  ==============================================================
         id      id of document, e.g. "id18154" (as `str`)
-        token   a token in the dataset, e.g. "the" (as `str`)
+        token   a token in the dataset, e.g. "the" (as `list(str)`)
         author  author of the document that this token belongs to (as `str`)
         ======  ==============================================================
-
     """
+
+
+def full_without_highpass(path):
+    file =  open(path, "r")
+    data = json.load(file)
+    df = pd.DataFrame(data, columns=['id', 'token', 'author'])
+    df.token = df.token.apply(lambda s: s.lower())
+    df['token'] = df.token.apply(lambda s: [s[start:end] for start, end in NLTKWordTokenizer().span_tokenize(s)])
+
+    return df
+
+if __name__ == "__main__":
+    print(full_without_highpass("new_train.json").head())
