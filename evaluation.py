621db88954cfe4cd0b9c2669bba7d07c45b131bf
Tristan Kobusch <tristan.kobusch@gmail.com>
2023-03-01 17:09:45 +0100

    make some adjustments

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def evaluate(input):
+def evaluate(input, names):
     """Return dataframe with random data.
 
     Parameters
     ----------
-    no_rows : pd.DataFrame
+    input : pd.DataFrame
         Data frame with labels and results. Data columns are as follows:
 
         ======== ==============================================================
@@ -13,4 +13,6 @@ def evaluate(input):
         <method> predicted author of document with <method> (as `str`)
         ...
         ======== ==============================================================
+    names : list(str)
+        Names of the prediction methods
     """
